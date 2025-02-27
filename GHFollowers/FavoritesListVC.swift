74ec6cb62af6b619a5e150855c19c6963a82dabf
ssgushyonka <orangeee04@gmail.com>
2025-02-27 23:38:05 +0300

    Initial Commit

---
+++ GHFollowers/FavoritesListVC.swift
@@ -0,0 +1,16 @@
+//
+//  FavoritesListVC.swift
+//  GHFollowers
+//
+//  Created by Элина Борисова on 27.02.2025.
+//
+
+import UIKit
+
+class FavoritesListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBlue
+    }
+}
---
+++ GHFollowers/SearchVC.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  GHFollowers
+//
+//  Created by Элина Борисова on 27.02.2025.
+//
+
+import UIKit
+
+class SearchVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .purple
+    }
+}
+
